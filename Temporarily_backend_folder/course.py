64cc0c2733f52d7f73ff1a5ac7614ee08a4749e7
Terry <cwhiyiu@gmail.com>
2023-10-12 13:48:03 +0800

    Roughly capable to generate course object

--- Temporarily_backend_folder/course.py
+++ Temporarily_backend_folder/course.py
@@ -1,47 +1,80 @@
 import genCourse
 
-class course:
-    Topic = None
-    Subtopics = {
-        "week1": None,
-        "week2": None,
-        "week3": None,
-        "week4": None,
-    }
-    week_1_topics={
-        "Name":None,
-        "Introduction": None,
-        "Explanation": None,
-        "Examples":None
-    }
-    week_2_topics = {
-        "Name":None,
-        "Introduction": None,
-        "Explanation": None,
-        "Examples": None
-    }
-    week_3_topics = {
-        "Name":None,
-        "Introduction": None,
-        "Explanation": None,
-        "Examples": None
-    }
-    week_4_topics = {
-        "Name":None,
-        "Introduction": None,
-        "Explanation": None,
-        "Examples": None
-    }
-    learning_materials=None
-    history=None
-    SubtopicList = genCourse.GenSubtopics()
+class Course:
 
-    for i in range(1,5):
-        Subtopics[f"week{i}"]=(SubtopicList[i-1])
+    def __init__(self, topic):
+        self.Topic = topic
+        self.Subtopics = {
+            "week1": None,
+            "week2": None,
+            "week3": None,
+            "week4": None,
+        }
+        self.week_1_topics={
+            "Name":None,
+            "Introduction": None,
+            "Explanation": None,
+            "Examples":None
+        }
+        self.week_2_topics = {
+            "Name":None,
+            "Introduction": None,
+            "Explanation": None,
+            "Examples": None
+        }
+        self.week_3_topics = {
+            "Name":None,
+            "Introduction": None,
+            "Explanation": None,
+            "Examples": None
+        }
+        self.week_4_topics = {
+            "Name":None,
+            "Introduction": None,
+            "Explanation": None,
+            "Examples": None
+        }
+        learning_materials=None
+        history=None
 
 
+        def genSubtopics(topic):
+            SubtopicList = genCourse.GenSubtopics(topic)
+            for i in range(1, 5):
+                self.Subtopics[f"week{i}"] = (SubtopicList[i - 1])
 
-print(course.Subtopics)
+        def gen_week_1_topics(name):
+            self.week_1_topics["Name"]=name
+            self.week_1_topics["Introduction"], self.week_1_topics["Explanation"], self.week_1_topics["Examples"] = \
+                genCourse.genContents(name)
+
+        def gen_week_2_topics(name):
+            self.week_2_topics["Name"]=name
+            self.week_2_topics["Introduction"], self.week_2_topics["Explanation"], self.week_2_topics["Examples"] = \
+                genCourse.genContents(name)
+        def gen_week_3_topics(name):
+            self.week_3_topics["Name"]=name
+            self.week_3_topics["Introduction"], self.week_3_topics["Explanation"], self.week_3_topics["Examples"] = \
+                genCourse.genContents(name)
+        def gen_week_4_topics(name):
+            self.week_4_topics["Name"]=name
+            self.week_4_topics["Introduction"], self.week_4_topics["Explanation"], self.week_4_topics["Examples"] = \
+                genCourse.genContents(name)
+
+        genSubtopics(self.Topic)
+        gen_week_1_topics(self.Subtopics["week1"])
+        gen_week_2_topics(self.Subtopics[1])
+        gen_week_3_topics(self.Subtopics[2])
+        gen_week_4_topics(self.Subtopics[3])
+
+
+
+
+
+
+
+
+
 
 
 
--- Temporarily_backend_folder/genCourse.py
+++ Temporarily_backend_folder/genCourse.py
@@ -4,7 +4,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain.schema import BaseOutputParser
 
-def GenSubtopics():
+def GenSubtopics(topic):
 
     class CommaSeparatedListOutputParser(BaseOutputParser):
         """Parse the output of an LLM call to a comma-separated list."""
@@ -14,7 +14,7 @@ def GenSubtopics():
             """Parse the output of an LLM call."""
             return text.strip().split(", ")
 
-    chat_model = ChatOpenAI(temperature=0.9)
+    ChatOpenAI(temperature=0.9)
     template =  """You are a helpful assistant who generates comma separated lists.
                 A user will pass in a category, and you should generate 4 subtopics under a 4-week course of that category in a comma separated list.
                 ONLY return a comma separated list, and nothing more."""
@@ -27,6 +27,30 @@ def GenSubtopics():
     ])
 
     chain = chat_prompt | ChatOpenAI() | CommaSeparatedListOutputParser()
-    subtopics = chain.invoke({"text": "I want to learn data structures"})
+    subtopics = chain.invoke({"text": f"I want to learn {topic}"})
     return subtopics
+
+
+def genContents(topics):
+
+    ChatOpenAI(temperature=0.9)
+    template = '''You are a virtual teaching assistant designed to support deaf students in understanding teacher's speech.
+                You should engage with them,  provide deaf individuals learning resources that align with their interests and personal
+                development goals. The student will pass in an interested topic , and you should teach him the topics.
+                explain each part as clear as you can. Please answer the question in an objective but easy way.'''
+
+    human_template="{text}"
+
+    chat_prompt = ChatPromptTemplate.from_messages([
+        ("system", template),
+        ("human", human_template),
+    ])
+    llm = ChatOpenAI()
+    introduction = llm(chat_prompt.format_messages(text=f"Simple introduction of {topics} within 100 words."))
+    explanation = llm(chat_prompt.format_messages(text=f"Explain {topics} as clear as you can, at least 500 words."))
+    examples = llm(chat_prompt.format_messages(text=f"Provide 3 examples of {topics} and explain the examples clearly, step by step"))
+
+    return introduction.content, explanation.content, examples.content
+
+
 
