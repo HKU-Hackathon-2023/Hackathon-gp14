1e78c996f931f4197753580aa9005466727a3264
ckm66 <ckm0410@connect.hku.hk>
2023-10-14 01:56:07 +0800

    Course Structure Recommendation

--- Temporarily_backend_folder/course.py
+++ Temporarily_backend_folder/course.py
@@ -1,71 +1,40 @@
 import genCourse
 
 class Course:
+    course_list: list(str) # list - contain name of all created courses
+    course_count: int # int - reprsend course count 
 
-    def __init__(self, topic):
-        self.Topic = topic
-        self.Subtopics = {
-            "week1": None,
-            "week2": None,
-            "week3": None,
-            "week4": None,
+    def __init__(self, subject):
+        self.subject: str # String - Subject name from user input
+        self.course_name: str # String - name of the course instant
+        self.estimated_weeks: int # int - weeks needed/ estimated to complete the course
+        self.weekly_teaching_schedule: {
+            "week 1": {
+                "topic": str, # Topic for the week
+                "sub topic": list(), # [[sub_topic_1, completed?], ]
+                "chat history": None
+            },
+            "week 2": {
+                "topic": str, # Topic for the week
+                "sub topic": list(), # [[sub_topic_1, completed?], ]
+                "chat history": None
+            },
+            "week 3": {
+                "topic": str, # Topic for the week
+                "sub topic": list(), # [[sub_topic_1, completed?], ]
+                "chat history": None
+            },
+            "week 4": {
+                "topic": str, # Topic for the week
+                "sub topic": list(), # [[sub_topic_1, completed?], ...]
+                "chat history": None
+            }
         }
-        self.week_1_topics={
-            "Name":None,
-            "Introduction": None,
-            "Explanation": None,
-            "Examples":None
-        }
-        self.week_2_topics = {
-            "Name":None,
-            "Introduction": None,
-            "Explanation": None,
-            "Examples": None
-        }
-        self.week_3_topics = {
-            "Name":None,
-            "Introduction": None,
-            "Explanation": None,
-            "Examples": None
-        }
-        self.week_4_topics = {
-            "Name":None,
-            "Introduction": None,
-            "Explanation": None,
-            "Examples": None
-        }
-        learning_materials=None
-        history=None
+        self.teaching_materials = None
+
+    def create_course(self):
 
 
-        def genSubtopics(topic):
-            SubtopicList = genCourse.GenSubtopics(topic)
-            for i in range(1, 5):
-                self.Subtopics[f"week{i}"] = (SubtopicList[i - 1])
-
-        def gen_week_1_topics(name):
-            self.week_1_topics["Name"]=name
-            self.week_1_topics["Introduction"], self.week_1_topics["Explanation"], self.week_1_topics["Examples"] = \
-                genCourse.genContents(name)
-
-        def gen_week_2_topics(name):
-            self.week_2_topics["Name"]=name
-            self.week_2_topics["Introduction"], self.week_2_topics["Explanation"], self.week_2_topics["Examples"] = \
-                genCourse.genContents(name)
-        def gen_week_3_topics(name):
-            self.week_3_topics["Name"]=name
-            self.week_3_topics["Introduction"], self.week_3_topics["Explanation"], self.week_3_topics["Examples"] = \
-                genCourse.genContents(name)
-        def gen_week_4_topics(name):
-            self.week_4_topics["Name"]=name
-            self.week_4_topics["Introduction"], self.week_4_topics["Explanation"], self.week_4_topics["Examples"] = \
-                genCourse.genContents(name)
-
-        genSubtopics(self.Topic)
-        gen_week_1_topics(self.Subtopics["week1"])
-        gen_week_2_topics(self.Subtopics[1])
-        gen_week_3_topics(self.Subtopics[2])
-        gen_week_4_topics(self.Subtopics[3])
 
 
 
